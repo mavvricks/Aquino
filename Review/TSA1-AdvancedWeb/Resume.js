26675aba4a7ffcc52441495fe7894410797460fe
mavvricks <150794652+mavvricks@users.noreply.github.com>
2025-01-28 18:06:37 +0800

    Delete Review/TSA1-AdvancedWeb directory

--- Review/TSA1-AdvancedWeb/Resume.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("change-color-btn").addEventListener("click", function(){
-    const body = document.body;
-    body.classList.toggle("dark-mode");
-
-    this.textContent = body.classList.contains("dark-mode") ? "Switch to Light Mode" : "Switch to Dark Mode";
-})
